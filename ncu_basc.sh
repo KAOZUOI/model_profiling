4fb10c8445c8e7d12e01345593ad13312b6ad39a
kozo <kozoYang@outlook.com>
2025-04-18 13:48:58 +0000

    feat: ncu prof

--- ncu_basc.sh
+++ ncu_basc.sh
@@ -1,6 +1,7 @@
 ncu \
-  --target-processes all \
-  --kernel-regex "gemm|attention" \
-  --metrics smsp__sass_thread_inst_executed_op_ffma.sum \
-  --metrics l1tex__t_bytes_pipe_lsu_mem_global_op_ld.sum \
+  --kernel-name "nvjet_tst_64x8_64x16_2x1_v_bz_TNT" \
+  --metrics "smsp__sass_thread_inst_executed_op_ffma.sum,smsp__pipe_tensor_cycles_active.avg,dram__bytes.sum,l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum" \
+  --section SpeedOfLight \
+  --export roofline \
+  --force-overwrite \
   python llama3_sing.py
