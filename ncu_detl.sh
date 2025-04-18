5c05b98e9d4ebd6104ef015c9b6d5c61e1688dbd
kozo <kozoYang@outlook.com>
2025-04-18 14:13:50 +0000

    feat: ncu prof

--- ncu_detl.sh
+++ ncu_detl.sh
@@ -1,7 +1,9 @@
 ncu \
   --kernel-name "nvjet_tst_64x8_64x16_2x1_v_bz_TNT" \
-  --metrics "smsp__sass_thread_inst_executed_op_ffma.sum,smsp__pipe_tensor_cycles_active.avg,dram__bytes.sum,l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum" \
+  --set detailed \
   --section SpeedOfLight \
   --export ncu_out/roofline \
   --force-overwrite \
+  --launch-skip 0 \
+  --launch-count 1 \
   python llama3_sing.py
